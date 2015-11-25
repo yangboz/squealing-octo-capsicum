2afd56f142b439afe4c1f335202b2cf4b5014f92
Zhou Yangbo <zhou.yangbo@intellif.com>
2015-11-25 13:44:51 +0800

    ADDED:first share of source code with example

---
+++ squealing_octo_capsicum/src/main/java/util/JPAUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+/**
+ * Created by yangboz on 11/25/15.
+ */
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory emFactory;
+
+    static {
+        try {
+            emFactory = Persistence.createEntityManagerFactory("org.hibernate.events.jpa");
+        } catch (Throwable ex) {
+            System.err.println("Cannot create EntityManagerFactory.");
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static EntityManager createEntityManager() {
+        return emFactory.createEntityManager();
+    }
+
+    public static void close() {
+        emFactory.close();
+    }
+}
